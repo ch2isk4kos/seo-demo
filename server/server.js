b8dbda3a48f1297bd6b551beb2bd8c5010e753f0
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-11 17:46:56 -0400

    create server file and import modules

---
+++ server/server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+require("dotenv").config(); // loads environment variables
