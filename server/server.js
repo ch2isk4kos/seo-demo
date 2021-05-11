640895e6afdd34c9dcc372d4e03a988f4119eb5c
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-11 17:54:15 -0400

    apply api test endpoint and create listener on server

--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ const cors = require("cors");
 const morgan = require("morgan");
 require("dotenv").config(); // loads environment variables
 
+// environment variables
+const PORT = process.env.PORT || 8000;
+
 // application initialization
 const app = express();
 
@@ -13,3 +16,12 @@ app.use(morgan("dev")); // provides server information to the console
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
+
+// routes
+app.get("/api", (res, req) => {
+  res.json({ time: Date.now().toString() });
+});
+
+app.listen(PORT, () => {
+  console.log(`Express listening on ${PORT}`);
+});
