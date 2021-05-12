85a65e8aa7a00cbb17d511574f633b921d42ccc9
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-12 19:26:49 -0400

    add endpoint to test for protective routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { signup, signin } = require("../controllers/auth.js");
+const {
+  signup,
+  signin,
+  signout,
+  requireSignin,
+} = require("../controllers/auth.js");
 
 // validators
 const { runValidation } = require("../validators/index");
@@ -11,5 +16,13 @@ const {
 
 router.post("/signup", userSignupValidation, runValidation, signup);
 router.post("/signin", userSigninValidation, runValidation, signin);
+router.get("/signout", signout);
+
+// test
+router.get("/secret", requireSignin, (req, res) => {
+  res.json({
+    message: "This is a protected route that has restricted access.",
+  });
+});
 
 module.exports = router;
