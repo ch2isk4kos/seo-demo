c696cfc76341e4193cea1a767c5602b9469155af
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-12 17:59:57 -0400

    add validation export method for user sign in

--- server/validators/auth.js
+++ server/validators/auth.js
@@ -7,3 +7,10 @@ exports.userSignupValidation = [
     .isLength({ min: 6 })
     .withMessage("password must be min 6 characters"),
 ];
+
+exports.userSigninValidation = [
+  check("email").isEmail().withMessage("must be a valid email"),
+  check("password")
+    .isLength({ min: 6 })
+    .withMessage("password must be min 6 characters"),
+];
