6bc739f831e508faf8e6159c7646b112bc0c6446
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-11 21:25:43 -0400

    import and pass user signup validation middleware into endpoint parameters

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { signup } = require("../controllers/auth.js");
 
-router.post("/signup", signup);
+// validators
+const { runValidation } = require("../validators/index");
+const { userSignupValidation } = require("../validators/auth");
+
+router.post("/signup", userSignupValidation, runValidation, signup);
 
 module.exports = router;
