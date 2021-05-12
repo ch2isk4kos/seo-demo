c9dfc9717200c9d1e60f3bf390ca7dfd958d872d
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-11 21:32:17 -0400

    bug fix: revise parameter typo

--- server/validators/index.js
+++ server/validators/index.js
@@ -1,10 +1,10 @@
 const { validationResult } = require("express-validator");
 
 exports.runValidation = (req, res, next) => {
-  const errors = validate(req);
+  const errors = validationResult(req);
 
   if (!errors.isEmpty())
-    return res.status(422).json({ error: error.array()[0].msg });
+    return res.status(422).json({ error: errors.array()[0].msg });
 
   next();
 };
