2c168c8a2c8a16cd6a5d20d6f0bb2a5524923c06
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-11 21:24:35 -0400

    create middleware export to validate user signup properties

---
+++ server/validators/auth.js
@@ -0,0 +1,9 @@
+const { check } = require("express-validator");
+
+exports.userSignupValidation = [
+  check("name").not().isEmpty().withMessage("name is required"),
+  check("email").isEmail().withMessage("must be a valid email"),
+  check("password")
+    .isLength({ min: 6 })
+    .withMessage("password must be min 6 characters"),
+];
