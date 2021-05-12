b40552c36935ee2cfa5037f1194a06fa0a8d173c
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-12 17:17:18 -0400

    bug fix: revise mongoose class && timestamp property typo

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const crypto = require("crypto");
 
-const userSchema = new mongoose.Model(
+const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
@@ -15,13 +15,14 @@ const userSchema = new mongoose.Model(
     name: {
       type: String,
       trim: true,
+      required: true,
       max: 32,
     },
     email: {
       type: String,
       trim: true,
       lowercase: true,
-      // index: true,
+      index: true,
       unique: true,
       required: true,
     },
@@ -50,7 +51,7 @@ const userSchema = new mongoose.Model(
       default: "",
     },
   },
-  { timeStamp: true }
+  { timestamp: true }
 );
 
 // virtual fields
