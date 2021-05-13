00b638a223c12d709a0d33ee8c8466c7d418b525
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-12 21:29:25 -0400

    import and render Header component

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "../components/Header";
 
 const Layout = ({ children }) => {
   return (
     <>
-      <p>Header</p>
+      <Header />
       {children}
       <p>Footer</p>
     </>
