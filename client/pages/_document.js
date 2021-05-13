36ce821e7a8f50e00afd35b33df510776c50e556
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-12 21:08:07 -0400

    add meta tags to custom next js document component

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -4,7 +4,13 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
