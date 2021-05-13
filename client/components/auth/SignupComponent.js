6bc8b0a99e452c20398b3f9a5b78da781f8fc89e
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-12 23:19:09 -0400

    render signup component form

--- client/components/auth/SignupComponent.js
+++ client/components/auth/SignupComponent.js
@@ -1,9 +1,52 @@
 import React from "react";
 
 const SignupComponent = () => {
+  const handleOnChange = (e) => {
+    console.log(e.target.value);
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log("handle on submit clicked");
+  };
+
+  const signupForm = () => {
+    return (
+      <form onSubmit={handleOnSubmit}>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="type your name"
+            onChange={handleOnChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="email"
+            placeholder="type your email"
+            onChange={handleOnChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="password"
+            placeholder="enter a password"
+            onChange={handleOnChange}
+          />
+        </div>
+        <div>
+          <button className="btn btn-primary">Submit</button>
+        </div>
+      </form>
+    );
+  };
   return (
     <>
       <h2>Sign Up Component</h2>
+      {signupForm()}
     </>
   );
 };
