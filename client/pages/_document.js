9a8fd9788c235e345d21f9ab39034fbb05d6ce48
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-12 21:10:15 -0400

    add bootstrap cdn link to customize css

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -10,6 +10,10 @@ class MyDocument extends Document {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css"
+          />
         </Head>
         <body>
           <Main />
