47af6ab627d5566b717d10fd0f0586a83ece6bdb
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-14 13:39:42 -0400

    import and render signin component

--- client/pages/signin.js
+++ client/pages/signin.js
@@ -1,13 +1,25 @@
 import Layout from "../components/Layout";
+import SigninComponent from "../components/auth/SigninComponent";
 import Link from "next/link";
 
 const Signin = () => {
   return (
     <Layout>
-      <h2>Sign In.</h2>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
+      <h2 className="text-center pt-4 pb-4">Sign In</h2>
+      <div className="row">
+        {/* <div className="col-md-8 offset-md-2"> */}
+        <div className="col-md-6 offset-md-3">
+          <SigninComponent />
+        </div>
+      </div>
+      <div className="row">
+        {/* <div className="col-md-8 offset-md-2 mt-2"> */}
+        <div className="col-md-6 offset-md-3 mt-2">
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </div>
+      </div>
     </Layout>
   );
 };
