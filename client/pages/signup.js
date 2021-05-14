23528cbac5bb2c8fd951aa0834a92698e81c292b
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-14 13:28:41 -0400

    apply padding to header tag

--- client/pages/signup.js
+++ client/pages/signup.js
@@ -5,6 +5,7 @@ import Link from "next/link";
 const Signup = () => {
   return (
     <Layout>
+      <h2 className="text-center pt-4 pb-4">Signup.</h2>
       <div className="row">
         {/* <div className="col-md-8 offset-md-2"> */}
         <div className="col-md-6 offset-md-3">
