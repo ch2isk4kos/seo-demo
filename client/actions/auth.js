2e085625b9f0036c45e72544106261979e705e09
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-14 13:31:39 -0400

    add fetch request method for user signin

--- client/actions/auth.js
+++ client/actions/auth.js
@@ -17,3 +17,20 @@ export const signup = (user) => {
       console.log(err);
     });
 };
+
+export const signin = (user) => {
+  return fetch(`${API}/api/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
