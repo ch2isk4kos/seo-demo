51ae5495a8b70e863909f78f777aa23e643b948b
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-14 13:04:16 -0400

    structure positioning of sign up component

--- client/pages/signup.js
+++ client/pages/signup.js
@@ -6,12 +6,14 @@ const Signup = () => {
   return (
     <Layout>
       <div className="row">
-        <div className="col-md-8 offset-md-2">
+        {/* <div className="col-md-8 offset-md-2"> */}
+        <div className="col-md-6 offset-md-3">
           <SignupComponent />
         </div>
       </div>
       <div className="row">
-        <div className="col-md-8 offset-md-2 mt-2">
+        {/* <div className="col-md-8 offset-md-2 mt-2"> */}
+        <div className="col-md-6 offset-md-3 mt-2">
           <Link href="/">
             <a>Home</a>
           </Link>
